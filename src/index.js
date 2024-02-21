1e75cd97c61f73c2fe8c255f1bf5018d807d92ac
Rolf Matela <rolffreitas@yahoo.com.br>
2024-02-21 00:11:26 -0300

    feat: cria as rotas POST e PATCH
    criação das rotas de POST e PATCH para a aplicação

--- src/index.js
+++ src/index.js
@@ -27,6 +27,30 @@ app.get('/racao', async (request, response) => {
     response.json(test);
 })
 
+app.post('/racao', async (request, response) => {
+    const cost = request.body;
+
+    await racao.insertOne(cost);
+    const allCosts = await racao.find().toArray();
+
+    response.json(allCosts);
+})
+
+app.patch('/racao', async (request, response) => {
+    const { id, ...updatedData } = request.body;
+
+    const result = await racao.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+    )
+
+    if (result.modifiedCount === 1) {
+        response.json({ message: "tudo show" })
+    } else {
+        response.status(404).json({ error: "tudo não show" })
+    }
+})
+
 app.listen(port, () => {
     console.log(`Aplicação iniciada com sucesso na porta ${port}`);
 })
