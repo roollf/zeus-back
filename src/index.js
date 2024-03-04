1e8b1bc2ca0dcc7aaa2f87f372650783084f0ccb
Rolf Matela <rolffreitas@yahoo.com.br>
2024-03-04 15:02:03 -0300

    feat: cria a rota DELETE
    criação da rota DELETE e atualização do código das rotas restantes

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ const uri = process.env.DB_CONNECTION_STRING;
 const app = express();
 const port = 3001;
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 app.use(express.json());
@@ -19,38 +19,51 @@ app.use(cors());
 
 client.connect();
 
-const zeus = client.db("zeus");
-const racao = zeus.collection("racao");
+const zeus = client.db('zeus');
+const racao = zeus.collection('racao');
 
 app.get('/racao', async (request, response) => {
-    const test = await racao.find().toArray();
-    response.json(test);
-})
+  const test = await racao.find().toArray();
+  response.json(test);
+});
 
 app.post('/racao', async (request, response) => {
-    const cost = request.body;
+  const cost = request.body;
 
-    await racao.insertOne(cost);
-    const allCosts = await racao.find().toArray();
+  await racao.insertOne(cost);
+  const allCosts = await racao.find().toArray();
 
-    response.json(allCosts);
-})
+  response.json(allCosts);
+});
 
 app.patch('/racao', async (request, response) => {
-    const { id, ...updatedData } = request.body;
+  const { id, ...updatedData } = request.body;
 
-    const result = await racao.updateOne(
-        { _id: new ObjectId(id) },
-        { $set: updatedData }
-    )
+  const result = await racao.updateOne({ _id: new ObjectId(id) }, { $set: updatedData });
 
-    if (result.modifiedCount === 1) {
-        response.json({ message: "tudo show" })
-    } else {
-        response.status(404).json({ error: "tudo não show" })
-    }
-})
+  if (result.modifiedCount === 1) {
+    response.json({ message: 'Documento atualizado com sucesso' });
+  } else {
+    response.status(200).json({ error: 'Documento não encontrado' });
+  }
+});
+
+app.delete('/racao', async (request, response) => {
+  const { id } = request.body;
+
+  if (!id) {
+    return response.status(400).json({ error: 'ID não fornecido' });
+  }
+
+  const result = await racao.deleteOne({ _id: new ObjectId(id) });
+
+  if (result.deletedCount === 1) {
+    response.json({ message: 'Documento excluído com sucesso' });
+  } else {
+    response.status(404).json({ error: 'Documento não encontrado' });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Aplicação iniciada com sucesso na porta ${port}`);
-})
+  console.log(`Aplicação iniciada com sucesso na porta ${port}`);
+});
